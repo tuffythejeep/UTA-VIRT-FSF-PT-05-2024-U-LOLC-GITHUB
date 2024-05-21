ed66b65e60e5fc1df62e810037fae8025d57730d
tuffythejeep <tuffythejeep@yahoo.com>
2024-05-20 20:25:20 -0500

    updated js

--- 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
+++ 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -7,4 +7,4 @@ let funFact = "Family Fun Fare in Florida";
 
 console.log("My name is", personName);
 console.log("I have", pets, "pets");
-console.log("Fun fact:", funFact)
+console.log("Fun fact:", funFact);
