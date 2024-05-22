55d1c96f54dbbfbcd4983624ee3c3331e57f841d
tuffythejeep <tuffythejeep@yahoo.com>
2024-05-21 21:41:55 -0500

    functions

--- 03-JavaScript/01-Activities/16-Stu_Functions/Unsolved/script.js
+++ 03-JavaScript/01-Activities/16-Stu_Functions/Unsolved/script.js
@@ -16,7 +16,8 @@ isEqual(10, 10);
 // Refer to sample functions as needed!
 
 // TODO: Call the isEqual function so that it logs "They are equal in value"
-
+isEqual(10, "10")
 // TODO: Rewrite isEqual as a function expression called 'isEqualTakeTwo`
 
 // TODO: Call the isEqualTakeTwo function so that it logs "They are not equal"
+isEqual(10 !== "10")
