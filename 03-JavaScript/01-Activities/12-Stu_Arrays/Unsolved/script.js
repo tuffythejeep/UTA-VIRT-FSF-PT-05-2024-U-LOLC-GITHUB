bf7d3718c1a926611e4fb5d4a73481278c4146fc
tuffythejeep <tuffythejeep@yahoo.com>
2024-05-21 20:06:45 -0500

    arrays

--- 03-JavaScript/01-Activities/12-Stu_Arrays/Unsolved/script.js
+++ 03-JavaScript/01-Activities/12-Stu_Arrays/Unsolved/script.js
@@ -1,1 +1,17 @@
 // WRITE YOUR CODE HERE
+const names = ["Alex", "Destinee", "Travis", "John"];
+
+console.log("Welcome to the class " + names[0])
+console.log("Welcome to the class " + names[1]);
+console.log("Welcome to the class " + names[2]);
+console.log("Welcome to the class " + names[3]);
+
+console.log(names.length);
+
+names[0] = "Roy";
+
+if (names[0] === "Roy") {
+    console.log(names[0] + " is in class!");
+}
+
+
