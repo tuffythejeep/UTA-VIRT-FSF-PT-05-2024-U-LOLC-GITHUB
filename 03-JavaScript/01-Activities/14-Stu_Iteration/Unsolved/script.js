302c76797c799624ff7eb1528577158b1ebc03e7
tuffythejeep <tuffythejeep@yahoo.com>
2024-05-21 21:04:46 -0500

    for loop

--- 03-JavaScript/01-Activities/14-Stu_Iteration/Unsolved/script.js
+++ 03-JavaScript/01-Activities/14-Stu_Iteration/Unsolved/script.js
@@ -1,1 +1,14 @@
 // WRITE YOUR CODE BELOW
+const students = ["Alex", "Destinee", "Travis", "John", "Sally"];
+
+console.log("Welcome to the class " + students[0]);
+console.log("Welcome to the class " + students[1]);
+console.log("Welcome to the class " + students[2]);
+console.log("Welcome to the class " + students[3]);
+console.log("Welcome to the class " + students[4]);
+
+console.log("Total number of students is " + students.length);
+
+for (let i = 0; i < students.length; i++) {
+  console.log(`Great to see you, ${students[i]}.`);
+}
