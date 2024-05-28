e39ce0ff6c16f1d58754cc98e3a80987f48cd888
tuffythejeep <tuffythejeep@yahoo.com>
2024-05-28 18:16:22 -0500

    added 04-Web-APIs

--- 03-JavaScript/01-Activities/20-Stu_Methods/Unsolved/script.js
+++ 03-JavaScript/01-Activities/20-Stu_Methods/Unsolved/script.js
@@ -3,3 +3,16 @@ const planets = ["Earth", "Saturn", "Mars", "Jupiter", "Uranus", "Venus"];
 const star = "polaris";
 
 // WRITE YOUR CODE BELOW
+constellations.unshift("Canis Major")
+
+console.log(constellations)
+
+planets.splice(5,1)
+console.log(planets)
+
+planets.pop
+console.log(planets);
+
+
+
+
--- 03-JavaScript/01-Activities/22-Stu_Objects/Unsolved/script.js
+++ 03-JavaScript/01-Activities/22-Stu_Objects/Unsolved/script.js
@@ -1,1 +1,17 @@
 // WRITE YOUR CODE BELOW
+const customerDrink = {
+    drinkName: "Venti Mocha",
+    numberOfSugar: "Two Stevia",
+    isOrderReady: false
+}
+
+console.log(customerDrink.drinkName)
+console.log(customerDrink.numberOfSugar);
+
+
+if (customerDrink.isOrderReady == true) {
+    console.log("Order Ready for Pickup");
+} else {
+    console.log("Still in order queue")
+}
+    
