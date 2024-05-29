a9037a1ad4815f04a45dd9337e0545def24f987a
tuffythejeep <tuffythejeep@yahoo.com>
2024-05-28 21:46:22 -0500

    Jax the cat

--- 04-Web-APIs/01-Activities/08-Stu_Create-Append/Unsolved/script.js
+++ 04-Web-APIs/01-Activities/08-Stu_Create-Append/Unsolved/script.js
@@ -5,8 +5,10 @@ const imgEl = document.createElement('img');
 const kittenEl = document.createElement('div');
 const nameEl = document.createElement('div');
 const favoriteEl = document.createElement('div');
+
 // Create ordered list element
 const listEl = document.createElement('ol');
+
 // Create ordered list items
 const li1 = document.createElement('li');
 const li2 = document.createElement('li');
@@ -24,6 +26,7 @@ infoEl.appendChild(imgEl);
 infoEl.appendChild(kittenEl);
 infoEl.appendChild(nameEl);
 body.appendChild(favoriteEl);
+
 // Append ordered list
 favoriteEl.appendChild(listEl);
 
@@ -35,3 +38,18 @@ kittenEl.setAttribute('style', 'font-size:25px; text-align:center;');
 favoriteEl.setAttribute('style', 'font-size:20px;');
 
 // TODO: Add ordered list items containing four favorite foods
+
+listEl.appendChild(li1);
+listEl.appendChild(li2);
+listEl.appendChild(li3);
+listEl.appendChild(li4);
+
+li1.textContent = "Burger";
+li2.textContent = "Pizza";
+li3.textContent = "Tacos";
+li4.textContent = "Cake";
+
+li1.setAttribute("style", "background-color: lightGrey;");
+li2.setAttribute("style", "background-color: grey;");
+li3.setAttribute("style", "background-color: darkGrey;");
+li4.setAttribute("style", "background-color: black; color: orange")
