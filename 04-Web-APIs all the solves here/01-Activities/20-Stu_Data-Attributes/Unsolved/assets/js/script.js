fb438d4f4a79f225d6d9408d0a5cd91f66801a97
tuffythejeep <tuffythejeep@yahoo.com>
2024-06-03 19:48:12 -0500

    adjusted to correct solution

--- 04-Web-APIs all the solves here/01-Activities/20-Stu_Data-Attributes/Unsolved/assets/js/script.js
+++ 04-Web-APIs all the solves here/01-Activities/20-Stu_Data-Attributes/Unsolved/assets/js/script.js
@@ -4,4 +4,11 @@ container.addEventListener('click', function (event) {
   const element = event.target;
 
   // TODO: Complete function
+  if (element.matches("div.box")) {
+    if (element.dataset.state === "hidden") {
+      const num = element.dataset.number;
+      element.dataset.state = "visible";
+      element.textContent = num;
+     }  
+  }
 });
