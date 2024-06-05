2f0d054d795dcad2c70c76ea0c48b41a0e4c6076
tuffythejeep <tuffythejeep@yahoo.com>
2024-06-04 20:34:11 -0500

    forms

--- 05-Third-Party-APIs/01-Activities/06-Stu_Form-Elements/Unsolved/assets/js/script.js
+++ 05-Third-Party-APIs/01-Activities/06-Stu_Form-Elements/Unsolved/assets/js/script.js
@@ -1,6 +1,18 @@
 const shoppingFormEl = $('#shopping-form');
 const shoppingListEl = $('#shopping-list');
-
+const shoppingInput = $('#shopping-input');
 // TODO: Create a function to handle the form submission event that captures the form's `<input>` value and prints it to the `shoppingListEl` as a `<li>`
+function handleFormSubmit(event) {
+event.preventDefault()
+
+console.log('item', shoppingInput.val());
+
+const item = $('<li>');
+item.text(shoppingInput.val());
+
+shoppingListEl.append(item);
+
+}
 
 // TODO: Add an event listener to the `shoppingFormEl` to handle submission
+shoppingFormEl.on("submit", handleFormSubmit);
