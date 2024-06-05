a2d3d686496a5e3dcc5156bc4173e8616222b995
tuffythejeep <tuffythejeep@yahoo.com>
2024-06-04 21:24:14 -0500

    forms

--- 05-Third-Party-APIs/01-Activities/08-Stu_Dom-Traversal/Unsolved/assets/js/script.js
+++ 05-Third-Party-APIs/01-Activities/08-Stu_Dom-Traversal/Unsolved/assets/js/script.js
@@ -2,5 +2,7 @@
 const rootEl = $('#root');
 
 // TODO: Starting from the root element, `rootEl`, make all the background color for each box white.
+rootEl.children("ul").children("li").css("background", "white");
 
 // TODO: Starting from `rootEl`, create the jQuery statement that will add "O" to block the "X" from winning
+rootEl.children("ul").children(".item-a3").text("O");
