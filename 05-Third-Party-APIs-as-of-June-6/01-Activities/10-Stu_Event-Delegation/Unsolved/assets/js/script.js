6959c09347b326a5e32fc825461bcf1436951b7f
tuffythejeep <tuffythejeep@yahoo.com>
2024-06-06 19:22:40 -0500

    event delegation

--- 05-Third-Party-APIs-as-of-June-6/01-Activities/10-Stu_Event-Delegation/Unsolved/assets/js/script.js
+++ 05-Third-Party-APIs-as-of-June-6/01-Activities/10-Stu_Event-Delegation/Unsolved/assets/js/script.js
@@ -29,7 +29,16 @@ function handleFormSubmit(event) {
 }
 
 // TODO: Create a function to handle removing a list item when `.delete-item-btn` is clicked
+function handleRemoveItem(event) {
+  const clickedBtn = $(event.target);
 
-// TODO: Use event delegation and add an event listener to `shoppingListEl` to listen for a click event on any element with a class of `.delete-item-btn` and execute the function created above
+  clickedBtn.parent().remove();
+}
 
-shoppingFormEl.on('submit', handleFormSubmit);
+
+// TODO: Use event delegation and add an event listener 
+//to `shoppingListEl` to listen for a click event on 
+//any element with a class of `.delete-item-btn` and execute the function created above
+shoppingListEl.on('click', '.delete-item-btn', handleRemoveItem);
+
+shoppingFormEl.on("submit", removeFromList);
