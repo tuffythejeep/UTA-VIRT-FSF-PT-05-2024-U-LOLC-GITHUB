5747ea3bd50f8c532d1d8ed28fdc756d87a155ae
tuffythejeep <tuffythejeep@yahoo.com>
2024-06-10 20:04:58 -0500

    updated jquery in script.js

--- 05-Third-Party-APIs-as-of-June-6/01-Activities/22-Stu_jQuery-UI-Widgets/Unsolved/assets/js/script.js
+++ 05-Third-Party-APIs-as-of-June-6/01-Activities/22-Stu_jQuery-UI-Widgets/Unsolved/assets/js/script.js
@@ -30,7 +30,38 @@ const handleFormSubmit = function (event) {
 formEl.on('submit', handleFormSubmit);
 
 // Add Autocomplete widget here
-//
+
+  $( function() {
+    var availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+    $( "#skill-name" ).autocomplete({
+      source: availableTags
+    });
+  });
 
 // Add Datepicker widget here
-//
+  $( function() {
+    $( "#datepicker" ).datepicker();
+  });
