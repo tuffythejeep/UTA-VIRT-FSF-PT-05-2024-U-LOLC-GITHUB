6f4f7d22172532c4fc25f098abf34a54820542bb
tuffythejeep <tuffythejeep@yahoo.com>
2024-06-11 21:20:33 -0500

    parse JSON

--- 06-Server-Side-APIs/01-Activities/08-Stu_Parse_JSON/Unsolved/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/08-Stu_Parse_JSON/Unsolved/assets/js/script.js
@@ -1,5 +1,5 @@
 // TODO: Edit the URL to get only 5 issues of Twitter's Chill repo
-const requestUrl = 'https://api.github.com/repos/{owner}/{repo}/issues';
+const requestUrl = 'https://api.github.com/repos/twitter/chill/issues?per_page=5';
 
 fetch(requestUrl)
   .then(function (response) {
@@ -9,5 +9,10 @@ fetch(requestUrl)
     console.log('Github Repo Issues \n----------');
     console.log(data);
     // TODO: Loop through the response
+    for (let i = 0; i < data.length; i++) {
+      console.log(data[i].name);
     // TODO: Console log each issue's URL and each user's login
+    console.log(data[i].url);
+    console.log(data[i].user.login);
+    }
   });
