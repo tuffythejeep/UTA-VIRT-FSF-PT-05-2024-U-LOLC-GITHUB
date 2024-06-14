c92d120bd545b3a49bfa139aeba0d1f1c710d5bf
tuffythejeep <tuffythejeep@yahoo.com>
2024-06-13 21:25:44 -0500

    6.20

--- 06-Server-Side-APIs/01-Activities/20-Stu_Document_Location/Unsolved/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/20-Stu_Document_Location/Unsolved/assets/js/script.js
@@ -1,7 +1,10 @@
-const badRequestUrl = 'https://api.github.com/unicorns';
-const redirectUrl = './404.html';
+const badRequestUrl = "https://api.github.com/unicorns";
+const redirectUrl = "./404.html";
 
 fetch(badRequestUrl).then(function (response) {
   // Use a conditional to check the response status.
   // If that status equals the conditional, then redirect to the 404 page.
+  if (!response.ok) {
+    window.location.replace(redirectUrl);
+  }
 });
