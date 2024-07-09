1905d2f918bec5214db3e2f5ff388e771363f53b
tuffythejeep <tuffythejeep@yahoo.com>
2024-07-08 21:20:40 -0500

    updates

--- 09-NodeJS/01-Activities/01-Ins_Node/index.js
+++ 09-NodeJS/01-Activities/01-Ins_Node/index.js
@@ -2,3 +2,4 @@
 (function () {
   console.log(this);
 })();
+
--- 09-NodeJS/01-Activities/18-Stu_Modularization/Unsolved/index.js
+++ 09-NodeJS/01-Activities/18-Stu_Modularization/Unsolved/index.js
@@ -1,7 +1,15 @@
 // TODO: Import `basicmath.js`
+module.exports = {
+  sum: (a, b) => a + b,
+  difference: (a, b) => a - b,
+  product: (a, b) => a * b,
+  quotient: (a, b) => a / b,
+};
 
 // TODO: Capture the values passed from the command line into these three variables: `operation`, `numOne` and `numTwo`
-
+const operation = process.argv[2];
+const numOne = parseInt(process.argv[3]);
+const numTwo = parseInt(process.argv[4]);
 // TODO: Create a `switch` statement that accepts an `operation` parameter
 // and each `case` uses the corresponding `maths` method
 // to perform each math operation on the two numbers, `numOne` and `numTwo`
