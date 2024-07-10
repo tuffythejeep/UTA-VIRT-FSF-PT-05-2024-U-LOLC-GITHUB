aa5e388931a7f98aded50bc5f0192afa86883aec
tuffythejeep <tuffythejeep@yahoo.com>
2024-07-09 21:22:27 -0500

    stuff

--- 09-NodeJS/01-Activities/28-Stu_Mini-Project/index.js
+++ 09-NodeJS/01-Activities/28-Stu_Mini-Project/index.js
@@ -1,17 +1,3 @@
-// TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
-
 const inquirer = require("inquirer");
 const fs = require("fs");
 
@@ -49,4 +35,5 @@ inquirer
     }
   ])
   .then((response) => {
+fs.writeFile("index.html")
 };
