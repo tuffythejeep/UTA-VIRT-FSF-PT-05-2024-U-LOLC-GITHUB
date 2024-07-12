e97d37cf5d02870cde1f792cfaefa5b41ae9b827
tuffythejeep <tuffythejeep@yahoo.com>
2024-07-11 20:29:46 -0500

    updates

--- 10-OOP/01-Activities/02-Stu_Constructors/Unsolved/index.js
+++ 10-OOP/01-Activities/02-Stu_Constructors/Unsolved/index.js
@@ -1,7 +1,24 @@
 // TODO: Create a constructor function called 'BlogPost' that takes in 'authorName', 'title', 'text, and 'createdOn'
+function BlogPost(authorName, title, text, createdOn) {
+    this.authorName = authorName;
+    this.title = title;
+    this.text = text;
+    this.createdOn = createdOn;
+    this.printMetaData = function() {
+        console.log('Created by ${this.authorName} on ${this.createdOn}');
+    }
 
 // TODO: Include a method called 'printMetaData()' that prints a message in the console saying 'Created by (authorName) on (createdOn)'
 
+
 // TODO: Create a new object using the 'BlogPost' constructor
 
+const Post = new BlogPost(
+    'John Doe',
+    'My First Blog Post',
+    'This is my first blog post!',
+    '2022-01-01'
+)
+
 // TODO: Call the 'printMetaData()' method on the new object
+Post.printMetaData();
