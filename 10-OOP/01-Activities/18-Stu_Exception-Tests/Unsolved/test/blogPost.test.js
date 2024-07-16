b30590572baf3f0b36220c21fdffafe1524ed178
tuffythejeep <tuffythejeep@yahoo.com>
2024-07-16 18:54:04 -0500

    updates

--- 10-OOP/01-Activities/18-Stu_Exception-Tests/Unsolved/test/blogPost.test.js
+++ 10-OOP/01-Activities/18-Stu_Exception-Tests/Unsolved/test/blogPost.test.js
@@ -2,5 +2,18 @@ const BlogPost = require('../blogPost.js');
 
 describe('BlogPost', () => {
   // TODO: Write a test that verifies that BlogPost throws an error when the authorName provided is less than 2 characters long.
+  it('should throw an error when authorName provided to constructor is < 2 characters', () => {
+  // Arrange
+
+  // Act
+
+  // Assert
+  })
   // TODO: Write a test that verifies that BlogPost throws an error when the authorName provided does not only contain uppercase, lowercase, numbers, dashes, and underscores.
-});
+it('should throw an error when authorName provided to constructor is < 2 characters', () => {
+  // Arrange
+
+  // Act
+
+  // Assert
+  })
