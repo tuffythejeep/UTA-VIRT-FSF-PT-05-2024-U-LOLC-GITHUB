df8f49bbb8c93ebbf4ba1e53696a11c31dddc52e
tuffythejeep <tuffythejeep@yahoo.com>
2024-07-16 20:20:05 -0500

    updates

--- 10-OOP/01-Activities/18-Stu_Exception-Tests/Unsolved/test/blogPost.test.js
+++ 10-OOP/01-Activities/18-Stu_Exception-Tests/Unsolved/test/blogPost.test.js
@@ -1,14 +1,21 @@
 const BlogPost = require('../blogPost.js');
 
 describe('BlogPost', () => {
+  describe('constructor', () => {
   // TODO: Write a test that verifies that BlogPost throws an error when the authorName provided is less than 2 characters long.
   it('should throw an error when authorName provided to constructor is < 2 characters', () => {
   // Arrange
-
+const invaliInput ="J"; //single character should be invalid
   // Act
-
+let error;
+  const newPost = new BlogPost(invalidInput);
+  } catch (err) {
+    error = err;
+  }
   // Assert
-  })
+expect(error).toBeDefined();
+ex  
+});
   // TODO: Write a test that verifies that BlogPost throws an error when the authorName provided does not only contain uppercase, lowercase, numbers, dashes, and underscores.
 it('should throw an error when authorName provided to constructor is < 2 characters', () => {
   // Arrange
@@ -16,4 +23,4 @@ it('should throw an error when authorName provided to constructor is < 2 charact
   // Act
 
   // Assert
-  })
+  });
--- 10-OOP/01-Activities/22-Stu_Promise-Chain/Unsolved/index.js
+++ 10-OOP/01-Activities/22-Stu_Promise-Chain/Unsolved/index.js
@@ -7,11 +7,15 @@ const BlogPost = require('./lib/blogPost');
 readFile('./data/post.json', 'utf-8').then((json) => {
   console.log('Received data from post.json');
   // TODO: parse the json string and assign the resulting object to a variable
-  //
+  const parsedData = JSON.parse(json);
   // TODO: Use the BlogPost class to create a new BlogPost object and use its `render()` method to return an html string.
-  //
+  const blogPost = new BlogPost(data.title, data.text, data.autho, data.createdOn);
+  const html = blogPost.render();
   // TODO: Write the html to file
-  //
-});
-
+  return writeFile('./dist/post.html', html);
+  // TODO: Write the html to file
+  return writeFile("index.html", html)
+  });
+.then(() => console.log ("File written successfully.");
+.catch((err) => console.log(err));
 // TODO: print a message in the console only after the html file is written.
