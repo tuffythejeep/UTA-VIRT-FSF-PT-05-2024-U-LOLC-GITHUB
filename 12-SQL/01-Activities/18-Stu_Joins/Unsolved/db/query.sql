e86aea4d214eebdafe57febf403e158bd4360567
tuffythejeep <tuffythejeep@yahoo.com>
2024-07-29 20:47:45 -0500

    updates

--- 12-SQL/01-Activities/18-Stu_Joins/Unsolved/db/query.sql
+++ 12-SQL/01-Activities/18-Stu_Joins/Unsolved/db/query.sql
@@ -1,1 +1,6 @@
 -- Add your code below and execute file in the PostgreSQL Shell --
+SELECT
+    *
+    FROM favorite_books
+    JOIN book_prices
+    ON favorite_books.book_price = book_prices.book_price
