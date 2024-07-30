abe019502724c5289cc39fcd8b74f44ca4711eef
tuffythejeep <tuffythejeep@yahoo.com>
2024-07-29 19:47:39 -0500

    updates

--- 12-SQL/01-Activities/16-Stu_Foreign-Primary-Key/Unsolved/db/schema.sql
+++ 12-SQL/01-Activities/16-Stu_Foreign-Primary-Key/Unsolved/db/schema.sql
@@ -1,1 +1,21 @@
 -- Write your Schema Here -- 
+DROP DATABASE IF EXISTS orders_db;
+CREATE DATABASE orders_db;
+
+\c orders_db;
+
+CREATE TABLE instructors (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30)
+);
+
+CREATE TABLE courses (
+  id SERIAL PRIMARY KEY,
+  course_title VARCHAR(30) NOT NULL,
+  instructor_id INTEGER,
+  course_description TEXT,
+  FOREIGN KEY (instructor_id)
+  REFERENCES instructors(id)
+  ON DELETE SET NULL
+);
