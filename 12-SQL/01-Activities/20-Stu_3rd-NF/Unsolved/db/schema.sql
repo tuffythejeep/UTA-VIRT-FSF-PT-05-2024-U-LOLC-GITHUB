49ec40cbea997cdcd097a462278a32a2c9dc419c
tuffythejeep <tuffythejeep@yahoo.com>
2024-07-29 21:35:41 -0500

    updates

--- 12-SQL/01-Activities/20-Stu_3rd-NF/Unsolved/db/schema.sql
+++ 12-SQL/01-Activities/20-Stu_3rd-NF/Unsolved/db/schema.sql
@@ -11,3 +11,13 @@ CREATE TABLE businesses (
     location_name VARCHAR(255),
     location_manager VARCHAR(255)
 );
+
+CREATE TABLE locations (
+    business_id INTEGER PRIMARY KEY,
+    business_name VARCHAR(255),
+    location_id INTEGER,
+    location_name VARCHAR(255),
+    location_manager VARCHAR(255)
+      FOREIGN KEY (business_id)
+      REFERENCES book_prices(id)
+      ON DELETE SET NULL
--- 12-SQL/01-Activities/20-Stu_3rd-NF/Unsolved/db/seeds.sql
+++ 12-SQL/01-Activities/20-Stu_3rd-NF/Unsolved/db/seeds.sql
@@ -1,8 +1,21 @@
 -- Inserting data into businesses
-INSERT INTO businesses (business_id, business_name, location_id, location_name, location_manager) VALUES
-(1, 'Prosser Lankovitch Products', 101, 'Annapolis, MD', 'Rita Ivanov'),
-(2, 'Ripley Enterprises', 102, 'Los Angeles, CA', 'Aaron Kamara'),
-(3, 'Gormenghast, Inc.', 103, 'Lincoln, NB', 'Farahd Mohamed'),
-(4, 'Nullhammer Corporation', 104, 'Saskatchewan, CA', 'Amara Marquez');
+INSERT INTO businesses (business_id, business_name) VALUES
+(1, 'Prosser Lankovitch Products'),
+(2, 'Ripley Enterprises'),
+(3, 'Gormenghast, Inc.'),
+(4, 'Nullhammer Corporation');
+
+INSERT INTO locations (location_id, location_name, location_manager) VALUES
+(101, 'Annapolis, MD', 'Rita Ivanov'),
+(102, 'Los Angeles, CA', 'Aaron Kamara'),
+(103, 'Lincoln, NB', 'Farahd Mohamed'),
+(104, 'Saskatchewan, CA', 'Amara Marquez');
+
+
 
 SELECT * FROM businesses;
+-- Query to retrieve student name, course name, and course instructor
+SELECT students.student_name, courses.course_name, courses.course_instructor 
+FROM students
+JOIN enrollments ON students.student_id = enrollments.student_id
+JOIN courses ON enrollments.course_id = courses.course_id;
