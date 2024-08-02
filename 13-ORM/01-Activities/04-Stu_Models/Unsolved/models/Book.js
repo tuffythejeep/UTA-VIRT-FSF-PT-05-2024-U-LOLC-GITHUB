735f489c56e8d2991438dfec117bd8760fb896d9
tuffythejeep <tuffythejeep@yahoo.com>
2024-08-01 21:06:26 -0500

    updates

--- 13-ORM/01-Activities/04-Stu_Models/Unsolved/models/Book.js
+++ 13-ORM/01-Activities/04-Stu_Models/Unsolved/models/Book.js
@@ -1,34 +1,40 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class Book extends Model {}
 
 Book.init(
   {
+    book_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     title: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
     },
     author: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
     },
     isbn: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
     },
     pages: {
-      type: DataTypes.INTEGER
+      type: DataTypes.INTEGER,
     },
     edition: {
-      type: DataTypes.INTEGER
+      type: DataTypes.INTEGER,
     },
     is_paperback: {
-      type: DataTypes.BOOLEAN
-    }
+      type: DataTypes.BOOLEAN,
+    },
   },
   {
     sequelize,
     timestamps: false,
     underscored: true,
-    modelName: 'book'
+    modelName: "book",
+    freezeTableName: true,
   }
 );
 
