717fabe49e3869b1a19c315bef96e8d560e78357
tuffythejeep <tuffythejeep@yahoo.com>
2024-08-08 20:24:06 -0500

    dish

--- 14-MVC/01-Activities/04-Stu_Handlebars-Expressions/Unsolved/controllers/dish-routes.js
+++ 14-MVC/01-Activities/04-Stu_Handlebars-Expressions/Unsolved/controllers/dish-routes.js
@@ -1,46 +1,49 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 // ? We are using hardcoded data here, where would our data usually come from? Remember - we haven't yet set up a database or Sequelize in our app.
 const dishes = [
   {
-    dish_name: 'French Bread with Brie Cheese',
-    description: 'French baguette with warm brie',
+    dish_name: "French Bread with Brie Cheese",
+    description: "French baguette with warm brie",
   },
   {
-    dish_name: 'Cheese Plate with Spanish Chorizo',
+    dish_name: "Cheese Plate with Spanish Chorizo",
     description:
-      'Manchego, Iberico, Cabrales, fig jam, grapes, pecans, and Spanish Chorizo',
+      "Manchego, Iberico, Cabrales, fig jam, grapes, pecans, and Spanish Chorizo",
   },
   {
-    dish_name: 'Fish Tacos',
+    dish_name: "Fish Tacos",
     description:
-      'Battered/fried fish, corn tortillas, fresh slaw with jalapeños and cilantro, pickled red onion',
+      "Battered/fried fish, corn tortillas, fresh slaw with jalapeños and cilantro, pickled red onion",
   },
   {
-    dish_name: 'Tropical Fruit Salad',
-    description: 'Guava, papaya, pineapple, mango, and star fruit',
+    dish_name: "Tropical Fruit Salad",
+    description: "Guava, papaya, pineapple, mango, and star fruit",
   },
   {
-    dish_name: 'Pork Gyoza',
+    dish_name: "Pork Gyoza",
     description:
-      'Homemade Japanese dumplings stuffed with a pork and green onion filling',
+      "Homemade Japanese dumplings stuffed with a pork and green onion filling",
   },
   {
-    dish_name: 'Yebeg Tibs with Injera Bread',
+    dish_name: "Yebeg Tibs with Injera Bread",
     description:
-      'Marinated lamb dish with rosemary, garlic, onion, tomato, jalapeño and the East African spice berbere',
+      "Marinated lamb dish with rosemary, garlic, onion, tomato, jalapeño and the East African spice berbere",
   },
   {
-    dish_name: 'Cape Malay Curry',
-    description: 'Chicken and vegetable curry dish with basmati rice',
+    dish_name: "Cape Malay Curry",
+    description: "Chicken and vegetable curry dish with basmati rice",
   },
 ];
 
 // ? Get all dishes
-router.get('/', async (req, res) => {
-  res.render('all');
+router.get("/", async (req, res) => {
+  res.render("all");
 });
 
-// TODO: Add a route called `/dish/:num` below
+//get one dish
+router.get('/dish/:num', async (req, res) => {
+  return res.render('dish', dishes[req.params.num - 1]);
+});
 
 module.exports = router;
